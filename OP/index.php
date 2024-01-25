9facf0d00751db7111364bfd500d4eb11ddc1d36
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-01-25 12:39:27 +0100

    added properties and construct method

---
+++ OP/index.php
@@ -0,0 +1,28 @@
+<!--Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.
+Microbonus: creo un'altra classe che aggiungo al pokemon per composizione che configuri il tipo di pokemon (aria, terra, ecc.)--> 
+
+<?php   
+
+class Pokemon {
+    public $name;
+    public $age;
+    public $evolution;
+    public $height;
+    public $weight;
+    public $color;
+
+    public function 
+
+    function __construct($_evolution) {
+        $this->evolution = $_evolution;
+        }
+}
+
+
+
+
+
+
+
+
+?>
